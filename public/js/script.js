7e9c04d855a63605fff4ca1c8c5e13ef99cf2afd
Vinicius Batista <vbanetyy@gmail.com>
2021-05-13 18:41:13 -0300

    Fixing delay

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,16 @@
-ScrollReveal().reveal('.headline', { delay: 500 });
+ScrollReveal().reveal('.headerMiddleyDelay', { delay: 350 });
 
-ScrollReveal().reveal('.headinfo', { delay: 1000 });
+ScrollReveal().reveal('.headerLeftDelay', { delay: 500 });
 
-ScrollReveal().reveal('.headmyself', { delay: 1500 });
+ScrollReveal().reveal('.headerRightDelay', { delay: 1000 });
 
-ScrollReveal().reveal('.headlineback', { delay: 1800 });
+ScrollReveal().reveal('.firstDelayback', { delay: 1500 });
+
+ScrollReveal().reveal('.thrirdDaley', { delay: 1800 });
+
+ScrollReveal().reveal('.headmyself', { delay: 1200 });
+
+ScrollReveal().reveal('.headlineback', { delay: 1200 });
 
 
 // ScrollReveal().reveal(target, options); 
