9a46b617b8c32e5854355c5378b4326ec4137a00
Vinicius Batista <vbanetyy@gmail.com>
2021-05-15 19:20:57 -0300

    Ajusting height of the page and insert effect on presentation

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,10 @@
-ScrollReveal().reveal('.headerMiddleyDelay', { delay: 250 });
+ScrollReveal().reveal('.headerLeftDelay', { delay: 250 });
 
-ScrollReveal().reveal('.headerLeftDelay', { delay: 350 });
+ScrollReveal().reveal('.headerRightDelay', { delay: 500 });
 
-ScrollReveal().reveal('.headerRightDelay', { delay: 600 });
+ScrollReveal().reveal('.headerMiddleyDelay', { delay: 750 });
 
-ScrollReveal().reveal('.firstDelayback', { delay: 800 });
+ScrollReveal().reveal('.firstDelayback', { delay: 1000 });
 
 ScrollReveal().reveal('.thrirdDaley', { delay: 1200 });
 
@@ -13,4 +13,19 @@ ScrollReveal().reveal('.headcollection', { delay: 1200 });
 ScrollReveal().reveal('.headlineback', { delay: 1200 });
 
 
-// ScrollReveal().reveal(target, options); 
+
+var classImage = document.createElement("div");
+    classImage.classList.add("my-image");
+
+function changeImage() {
+    document.querySelector(".img").style.display = 'none';
+    document.querySelector("#myImage").appendChild(classImage);   
+}
+
+function changeImg() {
+    document.querySelector(".img").style.display = 'block';
+    document.querySelector("#myImage").removeChild(classImage);
+        
+}
+
+console.log(changeImage())
