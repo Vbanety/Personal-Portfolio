dbd2a1f6dab9eaba430be3ad47c321e0f5f436af
Vinicius Batista <vbanetyy@gmail.com>
2021-07-07 19:56:28 -0300

    Fixing router

--- public/js/mail.js
+++ public/js/mail.js
@@ -4,7 +4,7 @@ const  mailGun = require('nodemailer-mailgun-transport');
 const auth = {
     auth: {
         api_key: 'f1437311f6188ee26831ceb471053125-c4d287b4-959fd3f4',
-        domain: 'sandboxa6ee6ba164bb4304a13f7f4e0009fb24.mailgun.org'
+        domain: 'sandboxa6ee6ba164bb4304a13f7f4e0009fb24.mailgun.org'        
     }
 };
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const sendMail = require('./public/js/mail.js')
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.use(express.static('public'));
