4c91d5718da4ecba4bb98b7490c31190954f3d63
Vinicius Batista <vbanetyy@gmail.com>
2021-07-08 07:11:23 -0300

    Changed server again.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-
 const express = require('express');
 const app = express();
 
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.use(express.static('public'));
 app.use(express.static('files'));
 
-
-app.use(express.static('public'))
 app.use(express.json())
 
 const port = process.env.PORT || 5000;
