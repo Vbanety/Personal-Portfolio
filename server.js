2456c4bd20653b44dede75414b88716b0d8138cf
Vinicius Batista <vbanetyy@gmail.com>
2021-07-08 06:21:47 -0300

    Remove function nodemailer

--- server.js
+++ server.js
@@ -2,45 +2,18 @@
 const express = require('express');
 const app = express();
 
-// const sendMail = require('./public/js/mail.js')
-
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
 })
 
 app.use(express.static('public'));
 app.use(express.static('files'));
 
-// app.use('/static', express.static('public'));
-// Middlewave
+
 app.use(express.static('public'))
 app.use(express.json())
-
-
-//part two
-// Data parsing
-// app.use(express.urlencoded({
-//     extended: false
-// }))
-// app.use(express.json());
-
-// app.post('/', (req, res) => {
-//     //TODO:
-//     // Send email here
-//     const { subject, email, text } = req.body;
-//     console.log('Data: ', req.body )
-
-//     sendMail(email, subject, text, function(err, data) {
-//         if(err) {
-//             res.status(500).json({message: 'Internal error'});
-//         } else {
-//             res.json({message: 'Email sent!!'});
-//         }
-//     });
-    
-// });
 
 const port = process.env.PORT || 5000;
 
